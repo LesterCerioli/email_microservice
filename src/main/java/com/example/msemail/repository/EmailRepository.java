e59b83aea3fc0fecb4ae015989fe785a3f683dbf
LesterCerioli <lesterlucasit@hotmail.com>
2022-06-05 16:09:36 -0400

    fet(#8): Setting Gmail sending email

--- src/main/java/com/example/msemail/repository/EmailRepository.java
+++ src/main/java/com/example/msemail/repository/EmailRepository.java
@@ -3,5 +3,7 @@ package com.example.msemail.repository;
 import com.example.msemail.models.EmailModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmailRepository extends JpaRepository<EmailModel, Long> {
+import java.util.UUID;
+
+public interface EmailRepository extends JpaRepository<EmailModel, UUID> {
 }
--- src/main/java/com/example/msemail/services/EmailService.java
+++ src/main/java/com/example/msemail/services/EmailService.java
@@ -1,9 +1,12 @@
 package com.example.msemail.services;
 
 
+import com.example.msemail.enums.StatusEmail;
 import com.example.msemail.models.EmailModel;
 import com.example.msemail.repository.EmailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +14,37 @@ public class EmailService {
     @Autowired
     EmailRepository emailRepository;
 
+    @Autowired
+    private JavaMailSender emailSender;
 
-    public void sendEmail(EmailModel emailModel) {
+
+    public EmailModel sendEmail(EmailModel emailModel) {
+
+        emailModel.setSendDateEmail(LocalDateTime.now());
+        try{
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(emailModel.getEmailFrom());
+            message.setTo(emailModel.getEmailTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getText());
+            emailSender.send(message);
+
+            emailModel.setStatusEmail(StatusEmail.SENT);
+
+        } catch (MailException e) {
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+
+        } finally {
+            return emailRepository.save(emailModel);
+        }
+
+    }
+
+    public Page<EmailModel> findAll(Pageable pageable) {
+        return emailRepository.findAll(Pageable pageable);
+    }
+
+    public Optional<EmailModel> findById(UID emailId) {
+        return emailRepository.findById(emailId);
     }
 }
